4ad381e0fa3e70b6efe03cae8a268e6e8094ca7a
Alison Golden <alisong779@yahoo.com>
2019-06-07 03:49:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
   get '/' do
     erb :user_input
   end
 
   post '/piglatinize' do
-    text_from_user = params[:user_text]
-    @analyzed_text = PigLatinizer.new(text_from_user)
-    erb :results
+    user_word = params["user_phrase"]
+    word = PigLatinizer.new
+    @pig_latinized_word = word.to_pig_latin(user_word)
+
+    erb :pit_latin
   end
 end
